4d13678c0d77af21db717d4a1aeacb71d261acb0
urieltan <urieltan3@gmail.com>
2018-04-07 11:44:34 +0800

    Initial commit

---
+++ app/src/main/java/com/urieltan/taxi_accounting/MainActivity.java
@@ -0,0 +1,13 @@
+package com.urieltan.taxi_accounting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
